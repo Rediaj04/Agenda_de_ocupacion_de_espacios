44ed3239a5eb8998ca703bee0ca2337934801fc0
Rediaj04 <j.a.cabarcas07@gmail.com>
2025-05-05 18:56:50 +0200

    Agregar archivos iniciales del proyecto, incluyendo configuración de Maven, controladores y propiedades de la aplicación.

---
+++ agenda/src/main/java/com/puig/BorradorController.java
@@ -0,0 +1,14 @@
+package com.puig.agenda;
+
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BorradorController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Aqui va la gracia de la agenda.............................";
+    }
+
+}
