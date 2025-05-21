8da7e1b78d741666f64f81bd06e624bc2ef707af
ireneurbano <ireneurbanoluque@gmail.com>
2025-05-21 16:32:24 +0200

    Css

--- agenda/src/main/java/com/puig/agenda/Controller/AgendaController.java
+++ agenda/src/main/java/com/puig/agenda/Controller/AgendaController.java
@@ -32,7 +32,7 @@ public class AgendaController {
         // Agregar salas al modelo
         model.addAttribute("salas", List.of(sala1, sala2));
         model.addAttribute("diasSemana", List.of("L", "M", "X", "J", "V", "S", "D"));
-        model.addAttribute("horas", List.of("08:00", "09:00", "10:00", "11:00", "12:00"));
+        model.addAttribute("horas", List.of("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"));
 
         return "agenda";
     }
